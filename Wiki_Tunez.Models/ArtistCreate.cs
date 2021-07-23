d6c21aa3939b67d74b972637e7695b52999f5127
julie <juliebishopphotos@gmail.com>
2021-07-23 11:48:23 -0400

    added model artist create

---
+++ Wiki_Tunez.Models/ArtistCreate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wiki_Tunez.Models
+{
+    public class ArtistCreate
+    {
+        public enum ArtistType
+        {
+            Solo,
+            Group,
+            Band
+        }
+        public string Name { get; set; }
+        public ArtistType TypeOfArtist { get; set; }
+    }
+}
