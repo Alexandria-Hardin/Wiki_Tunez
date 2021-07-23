6aa795cf5695d09ae076da417d85081802facab4
Alexandria Hardin <aphardin101@gmail.com>
2021-07-23 13:53:25 -0500

    rough draft of playlist models

---
+++ Wiki_Tunez.Models/PlaylistCreate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wiki_Tunez.Models
+{
+    public class PlaylistCreate
+    {
+        public string Name { get; set; }
+        public int SongId { get; set; }
+    }
+}
