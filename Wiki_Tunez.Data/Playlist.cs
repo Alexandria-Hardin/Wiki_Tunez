ce9a39137fe37aca1095220db85d13a5ca9b5356
Alexandria Hardin <aphardin101@gmail.com>
2021-07-23 10:14:37 -0500

    Added playlist class content

--- Wiki_Tunez.Data/Playlist.cs
+++ Wiki_Tunez.Data/Playlist.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,14 @@ namespace Wiki_Tunez.Data
 {
     public class Playlist
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public Guid UserId { get; set; }
+        [ForeignKey(nameOf(Song))]
+        public int SongId { get; set; }
+        public virtual Song Song { get; set; }
     }
 }
