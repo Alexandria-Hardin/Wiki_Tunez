02d01db3041ad1fc48602ec21bc42b6fd884243a
Alexandria Hardin <aphardin101@gmail.com>
2021-07-26 15:38:52 -0500

    rough draft of playlist controller

--- Wiki_Tunez.WebAPI/Controllers/PlaylistController.cs
+++ Wiki_Tunez.WebAPI/Controllers/PlaylistController.cs
@@ -4,10 +4,67 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using Wiki_Tunez.Models;
+using Wiki_Tunez.Services;
 
 namespace Wiki_Tunez.WebAPI.Controllers
 {
+    [Authorize]
     public class PlaylistController : ApiController
     {
+        public IHttpActionResult Get()
+        {
+            PlaylistService playlistService = CreatePlaylistService();
+            var playlists = playlistService.GetPlaylists();
+            return Ok(playlists);
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            PlaylistService playlistService = CreatePlaylistService();
+            var playlist = playlistService.GetPlaylistById(id);
+            return Ok(playlist);
+        }
+        public IHttpActionResult Get(Guid Id)
+        {
+            PlaylistService playlistService = CreatePlaylistService();
+            var playlist = playlistService.GetPlaylistByGuid(Id);
+            return Ok(playlist);
+        }
+
+        public IHttpActionResult Post(PlaylistCreate playlist)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreatePlaylistService();
+            if (!service.CreatePlaylist(playlist))
+                return InternalServerError();
+            return Ok();
+        }
+        private PlaylistService CreatePlaylistService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var noteService = new PlaylistService(userId);
+            return noteService;
+        }
+
+        public IHttpActionResult Put(PlaylistEdit playlist)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreatePlaylistService();
+            if (!service.UpdatePlaylist(playlist))
+                return InternalServerError();
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreatePlaylistService();
+            if (!service.DeletePlaylist(id))
+                return InternalServerError();
+            return Ok();
+        }
     }
 }
