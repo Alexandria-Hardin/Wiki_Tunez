437f66411c8d3b9c1ca633d2ec355ff6d62f9d87
nmalcock <nmalcock97@gmail.com>
2021-07-26 11:23:44 -0400

    Adding Song Models

---
+++ Wiki_Tunez.Models/SongCreate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wiki_Tunez.Data;
+
+namespace Wiki_Tunez.Models
+{
+    public class SongCreate
+    {
+        public int SongId { get; set; }
+
+        public string Title { get; set; }
+
+        public double RunTime { get; set; }
+
+        public int ArtistId { get; set; }
+
+        public GenreType TypeOfGenre { get; set; }
+
+    }
+}
