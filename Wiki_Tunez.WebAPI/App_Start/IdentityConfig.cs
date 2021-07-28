0d8b9386b8f92d9eba06ce4e0733e176e2110a9e
julie <juliebishopphotos@gmail.com>
2021-07-28 14:41:18 -0400

    initial migration

--- Wiki_Tunez.WebAPI/App_Start/IdentityConfig.cs
+++ Wiki_Tunez.WebAPI/App_Start/IdentityConfig.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
-using Wiki_Tunez.WebAPI.Data;
+using Wiki_Tunez.Data;
 using Wiki_Tunez.WebAPI.Models;
 
 namespace Wiki_Tunez.WebAPI
